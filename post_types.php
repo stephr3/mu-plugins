4c057c85a17d8838a5c4dc8274312651d8664b5b
Deana <demaisja@gmail.com>
2021-05-19 18:24:13 -0700

    includes teaching and real world learning resources

--- post_types.php
+++ post_types.php
@@ -29,6 +29,36 @@ function post_types() {
     ),
     'menu_icon' => 'dashicons-portfolio'
   ));
+
+  //Teaching Resources Post Type
+  register_post_type('teachingresource', array(
+    'rewrite' => array('slug' => 'teachingresources'),
+    'has_archive' => true,    
+    'public' => true,
+    'labels' => array(
+      'name' => 'Teacher Resources',
+      'add_new_item' => 'Add New Resource',
+      'edit_item' => 'Edit Resource',
+      'all_items' => 'All Resources',
+      'singular_name' => 'Teaching Resource'
+    ),
+    'menu_icon' => 'dashicons-edit-large'
+  ));
+
+  //Real World Learning Resources Post Type
+  register_post_type('realworldlearning', array(
+    'rewrite' => array('slug' => 'realworldlearning'),
+    'has_archive' => true,    
+    'public' => true,
+    'labels' => array(
+      'name' => 'Real World Learning Resources',
+      'add_new_item' => 'Add New Resource',
+      'edit_item' => 'Edit Resource',
+      'all_items' => 'All Resources',
+      'singular_name' => 'Real World Learning Resource'
+    ),
+    'menu_icon' => 'dashicons-location-alt'
+  ));
 }
 
 add_action('init', 'post_types');
